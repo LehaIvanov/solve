2795fb01ac87213eed35d2800227ea9a5ce4a43e
Алексей Иванов <alexey.ivanov@travelline.ru>
2023-09-19 18:53:27 +0300

    Init

---
+++ bin/solve.dart
@@ -0,0 +1,5 @@
+import 'package:solve/solve.dart' as solve;
+
+void main(List<String> arguments) {
+  print('Hello world: ${solve.calculate()}!');
+}
---
+++ test/solve_test.dart
@@ -0,0 +1,8 @@
+import 'package:solve/solve.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
